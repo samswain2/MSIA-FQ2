e01cc67c6a9f9dd64c521ec47043ad8f33990820
samswain2 <samswain2256@gmail.com>
2023-10-15 02:42:07 -0500

    Finished 490 hw01 q1 (above 195 average over 100 episodes)

--- 490_msia/01_hw/01_hw_code.py
+++ 490_msia/01_hw/01_hw_code.py
@@ -8,30 +8,54 @@ import tensorflow as tf
 # Suppress DeprecationWarnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-# List all GPUs available to TensorFlow
-gpus = tf.config.list_physical_devices('GPU')
+def check_gpu_availability():
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        print("GPU is available")
+        for gpu in gpus:
+            print(f"GPU device: {gpu}")
+    else:
+        print("GPU is not available")
 
-# Check if any GPUs are available
-if len(gpus) > 0:
-    print("GPU is available")
-    for gpu in gpus:
-        print(f"GPU device: {gpu}")
-else:
-    print("GPU is not available")
+def build_model(input_shape=(4,), num_choices=2, reg=0.0001):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(256, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(reg), input_shape=input_shape),
+        tf.keras.layers.Dense(128, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(reg)),
+        tf.keras.layers.Dense(num_choices, activation="softmax")
+    ])
+    return model
 
-# Initialize Environment and Model
-env = gym.make("CartPole-v0")
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(256, activation="relu", input_shape=(4,)),
-    tf.keras.layers.Dense(128, activation="relu"),
-    tf.keras.layers.Dense(2, activation="softmax")
-])
+def select_action(model, observation, num_choices=2):
+    probabilities = model.predict(np.array([observation]), verbose=0)[0]
+    return np.random.choice(num_choices, p=probabilities)
 
-def CartPole_RL(model, discount_factor=0.95, num_choices=2):
+def compute_discounted_rewards(reward_history, discount_factor=0.95):
+    discounted_rewards, cumulative_reward = [], 0
+    for reward in reversed(reward_history):
+        cumulative_reward = reward + discount_factor * cumulative_reward
+        discounted_rewards.insert(0, cumulative_reward)
+    return discounted_rewards
+
+def adjust_weights(model, optimizer, obs_history, action_history, discounted_rewards):
+    with tf.GradientTape() as tape:
+        probs=model(np.array(obs_history))
+        indices=tf.stack([tf.range(len(action_history),dtype=tf.int32),tf.convert_to_tensor(action_history,dtype=tf.int32)],axis=1)
+        chosen_probs=tf.gather_nd(probs,indices)
+        loss=-tf.math.log(chosen_probs)*discounted_rewards
+        loss=tf.reduce_mean(loss)
+    grads=tape.gradient(loss,model.trainable_variables)
+    optimizer.apply_gradients(zip(grads,model.trainable_variables))
+
+def CartPole_RL():
+    env = gym.make("CartPole-v0")
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
+    model = build_model()
     episode_rewards = []
     episode = 0
-    
+
+    consecutive_200_rewards = 0  # Count number of 200 occurences
+    should_train = True  # Initialize flag for training
+
     while True:
         observation, info = env.reset()
         obs_history, reward_history, action_history = [], [], []
@@ -40,48 +64,57 @@ def CartPole_RL(model, discount_factor=0.95, num_choices=2):
         truncated = False
 
         while not terminated and not truncated:
-            probabilities = model.predict(np.array([observation]), verbose=0)[0]
-            action = np.random.choice(num_choices, p=probabilities)
-
+            action = select_action(model, observation)
             obs_history.append(observation)
             action_history.append(action)
 
             observation, reward, terminated, truncated, info = env.step(action)
             reward_history.append(reward)
 
-        # Discount rewards
-        discounted_rewards = []
-        cumulative_reward = 0
-        for reward in reversed(reward_history):
-            cumulative_reward = reward + discount_factor * cumulative_reward
-            discounted_rewards.insert(0, cumulative_reward)
-
-        def adjust_weights(obs_history, action_history, discounted_rewards):
-            with tf.GradientTape() as tape:
-                action_probabilities = model(np.array(obs_history))
-                indices = list(zip(range(len(action_history)), action_history))
-                chosen_action_probs = tf.gather_nd(action_probabilities, indices)
-                loss = -tf.math.log(chosen_action_probs) * discounted_rewards
-                loss = tf.reduce_mean(loss)
-            grads = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
-        adjust_weights(obs_history, action_history, discounted_rewards)
+        discounted_rewards = compute_discounted_rewards(reward_history)
+        # adjust_weights(model, optimizer, obs_history, action_history, discounted_rewards)
 
         total_reward = sum(reward_history)
         episode_rewards.append(total_reward)
 
         moving_num, window = 195, 100
         if episode >= window-1:
-            moving_avg = np.mean(episode_rewards[-window:]) # Compute moving average
+            moving_avg = np.mean(episode_rewards[-window:])
             print(f"CartPole-v0 episode {episode}, reward sum: {total_reward}, last {window} avg: {moving_avg:.2f}")
             
             if moving_avg > moving_num:
                 print(f"Stopping as the last {window}-episode moving average is greater than {moving_num}")
                 break
-        
         else:
             print(f"CartPole-v0 episode {episode}, reward sum: {total_reward}")
+
+
+
+        ### TESTING TRAINING STOP
+
+        if total_reward == 200:  # Check for consecutive rewards of 200
+            consecutive_200_rewards += 1
+            if consecutive_200_rewards >= 10 and should_train == True:
+                print("Stopping training as the reward has been 200 for 10 episodes in a row")
+                should_train = False  # Set flag to False
+
+                # Check if folder exists, if not create it
+                if not os.path.exists("saved_model"):
+                    os.makedirs("saved_model")
+
+                # Save the model
+                model.save("saved_model/cartpole_model")
+                
+        else:
+            consecutive_200_rewards = 0  # Reset the counter if the reward is not 200
+
+        # Modify the weight adjustment to respect the should_train flag
+        if should_train:
+            adjust_weights(model, optimizer, obs_history, action_history, discounted_rewards)
+
+        ### TESTING TRAINING STOP
+
+
 
         episode += 1
 
@@ -96,4 +129,5 @@ def CartPole_RL(model, discount_factor=0.95, num_choices=2):
     plt.savefig('artifacts/episode_rewards.png')
 
 if __name__ == "__main__":
-    CartPole_RL(model)
+    check_gpu_availability()
+    CartPole_RL()
