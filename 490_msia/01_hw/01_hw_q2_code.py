cb5fdd76c59bf3abe9cf6c67f1b09ac75af4bfea
samswain2 <samswain2256@gmail.com>
2023-10-18 20:15:55 +0000

    Worked on hw 01 q2 490

--- 490_msia/01_hw/01_hw_q2_code.py
+++ 490_msia/01_hw/01_hw_q2_code.py
@@ -11,28 +11,40 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 def check_gpu_availability():
     gpus = tf.config.list_physical_devices('GPU')
     if gpus:
-        print("GPU is available")
+        print("GPU is available", flush=True)
         for gpu in gpus:
-            print(f"GPU device: {gpu}")
-    else:
-        print("GPU is not available")
+            print(f"GPU device: {gpu}", flush=True)
+            tf.config.experimental.set_memory_growth(gpu, True)
 
-def preprocess(image, downsample_factor=4):
-    """ prepro 210x160x3 uint8 frame into 1600 (40x40) 2D float array """
+        os.environ["CUDA_VISIBLE_DEVICES"] = "3" # Set the fourth GPU as available
+
+    else:
+        print("GPU is not available", flush=True)
+
+def preprocess(image, downsample_factor=2):
+    """ prepro 210x160x3 uint8 frame into 6400 (80x80) 2D float array """
     image = image[35:195] # crop
-    image = image[::downsample_factor,::downsample_factor,0] # downsample by factor of 4
+    image = image[::downsample_factor,::downsample_factor,0] # downsample by factor of 2
     image[image == 144] = 0 # erase background (background type 1)
     image[image == 109] = 0 # erase background (background type 2)
     image[image != 0] = 1 # everything else (paddles, ball) just set to 1
     # In preprocess function
-    return np.reshape(image.astype(np.float64).ravel(), [40, 40])
+    return tf.convert_to_tensor(np.reshape(image.astype(np.float32).ravel(), [80, 80, 1]))
 
 
-def build_model(input_shape=(40, 40, 1), num_choices=2, reg=0.0001):
+def build_model(input_shape=(80, 80, 1), num_choices=2, reg=0.0001):
     model = tf.keras.Sequential([
-        tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu", kernel_regularizer=tf.keras.regularizers.l2(reg), input_shape=input_shape),
-        tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu", kernel_regularizer=tf.keras.regularizers.l2(reg)),
+        tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu", kernel_regularizer=tf.keras.regularizers.l2(reg), input_shape=input_shape),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        
+        tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu", kernel_regularizer=tf.keras.regularizers.l2(reg)),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        
         tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dense(num_choices, activation="softmax")
     ])
     return model
@@ -47,17 +59,41 @@ def compute_discounted_rewards(reward_history, discount_factor=0.99):
     for reward in reversed(reward_history):
         cumulative_reward = reward + discount_factor * cumulative_reward
         discounted_rewards.insert(0, cumulative_reward)
-    return discounted_rewards
+    return tf.convert_to_tensor(discounted_rewards, dtype=tf.float32)
+
+# @tf.function(input_signature=[
+#     tf.TensorSpec(shape=[None, 80, 80, 1], dtype=tf.float32),
+#     tf.TensorSpec(shape=[None], dtype=tf.int32),
+#     tf.TensorSpec(shape=[None], dtype=tf.float32)
+# ])
+# def adjust_weights(model, optimizer, obs_history, action_history, discounted_rewards):
+#     with tf.GradientTape() as tape:
+#         probs=model(tf.convert_to_tensor(obs_history, dtype=tf.float32))
+#         indices=tf.stack([tf.range(len(action_history),dtype=tf.int32),tf.convert_to_tensor(action_history,dtype=tf.int32)],axis=1)
+#         chosen_probs=tf.gather_nd(probs,indices)
+#         loss=-tf.math.log(chosen_probs)*discounted_rewards
+#         loss=tf.reduce_mean(loss)
+#     grads=tape.gradient(loss,model.trainable_variables)
+#     optimizer.apply_gradients(zip(grads,model.trainable_variables))
 
 def adjust_weights(model, optimizer, obs_history, action_history, discounted_rewards):
-    with tf.GradientTape() as tape:
-        probs=model(np.array(obs_history))
-        indices=tf.stack([tf.range(len(action_history),dtype=tf.int32),tf.convert_to_tensor(action_history,dtype=tf.int32)],axis=1)
-        chosen_probs=tf.gather_nd(probs,indices)
-        loss=-tf.math.log(chosen_probs)*discounted_rewards
-        loss=tf.reduce_mean(loss)
-    grads=tape.gradient(loss,model.trainable_variables)
-    optimizer.apply_gradients(zip(grads,model.trainable_variables))
+    @tf.function(input_signature=[
+        tf.TensorSpec(shape=[None, 80, 80, 1], dtype=tf.float32),
+        tf.TensorSpec(shape=[None], dtype=tf.int32),
+        tf.TensorSpec(shape=[None], dtype=tf.float32)
+    ])
+    def inner_function(obs_history, action_history, discounted_rewards):
+        with tf.GradientTape() as tape:
+            probs = model(tf.convert_to_tensor(obs_history, dtype=tf.float32))
+            indices = tf.stack([tf.range(len(action_history), dtype=tf.int32),
+                                tf.convert_to_tensor(action_history, dtype=tf.int32)], axis=1)
+            chosen_probs = tf.gather_nd(probs, indices)
+            loss = -tf.math.log(chosen_probs) * discounted_rewards
+            loss = tf.reduce_sum(loss)
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        
+    inner_function(obs_history, action_history, discounted_rewards)
 
 def Pong_RL():
     env = gym.make("Pong-v0")
@@ -79,7 +115,7 @@ def Pong_RL():
         while not terminated and not truncated:
             processed_observation = preprocess(observation)
             action = select_action(model, processed_observation)
-            obs_history.append(processed_observation)
+            obs_history.append(tf.convert_to_tensor(processed_observation))
             action_history.append(action)
 
             observation, reward, terminated, truncated, info = env.step(action)
@@ -93,20 +129,20 @@ def Pong_RL():
         moving_num, window = 0, 100
         if episode >= window-1:
             moving_avg = np.mean(episode_rewards[-window:])
-            print(f"Pong-v0 episode {episode}, reward sum: {total_reward}, last {window} avg: {moving_avg:.2f}")
+            print(f"Pong-v0 episode {episode}, reward sum: {total_reward}, last {window} avg: {moving_avg:.2f}", flush=True)
             
             if moving_avg > moving_num:
-                print(f"Stopping as the last {window}-episode moving average is greater than {moving_num}")
+                print(f"Stopping as the last {window}-episode moving average is greater than {moving_num}", flush=True)
                 break
         else:
-            print(f"Pong-v0 episode {episode}, reward sum: {total_reward}")
+            print(f"Pong-v0 episode {episode}, reward sum: {total_reward}", flush=True)
 
         ### TRAINING STOP
 
         if total_reward == 21:  # Check for consecutive rewards of 21
             consecutive_21_rewards += 1
             if consecutive_21_rewards >= 10 and should_train == True:
-                print("Stopping training as the reward has been 21 for 10 episodes in a row")
+                print("Stopping training as the reward has been 21 for 10 episodes in a row", flush=True)
                 should_train = False  # Set flag to False
 
                 # Check if folder exists, if not create it
