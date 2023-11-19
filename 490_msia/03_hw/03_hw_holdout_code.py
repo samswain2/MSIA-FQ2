505e8540ed114c7fc62623d456c75d708dcf29e0
samswain2 <samswain2256@gmail.com>
2023-11-18 19:02:43 -0600

    Worked on hw03 490 and hw07 414

--- 490_msia/03_hw/03_hw_holdout_code.py
+++ 490_msia/03_hw/03_hw_holdout_code.py
@@ -1,14 +1,13 @@
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 class ModelEvaluator:
-    def __init__(self, model_path, test_data_path, output_file):
-        self.model_path = model_path
+    def __init__(self, test_data_path):
         self.test_data_path = test_data_path
-        self.output_file = output_file
 
-    def load_model(self):
-        return tf.keras.models.load_model(self.model_path)
+    def load_model(self, model_path):
+        return tf.keras.models.load_model(model_path)
 
     def load_test_data(self):
         test_data = np.load(self.test_data_path, allow_pickle=True)
@@ -20,24 +19,60 @@ class ModelEvaluator:
         test_loss, test_accuracy = model.evaluate(test_images, test_labels, verbose=0)
         return test_loss, test_accuracy
 
-    def write_results_to_file(self, test_loss, test_accuracy):
-        with open(self.output_file, 'w') as file:
-            file.write(f"Test Loss: {test_loss}\n")
-            file.write(f"Test Accuracy: {test_accuracy}\n")
-
-    def run_evaluation(self):
-        model = self.load_model()
+    def run_evaluation(self, model_path, output_file):
+        model = self.load_model(model_path)
         test_images, test_labels = self.load_test_data()
         test_loss, test_accuracy = self.evaluate_model(model, test_images, test_labels)
-        self.write_results_to_file(test_loss, test_accuracy)
-        print(f"Evaluation complete. Results written to {self.output_file}")
+
+        with open(output_file, 'a') as file:
+            file.write(f"Model: {model_path}\n")
+            file.write(f"Test Loss: {test_loss}\n")
+            file.write(f"Test Accuracy: {test_accuracy}\n\n")
+
+        print(f"Evaluation complete for {model_path}. Results written to {output_file}")
+        return test_loss, test_accuracy
+
+def plot_results(results, plot_file):
+    fractions = list(results.keys())
+    accuracies = [results[fraction]['accuracy'] for fraction in fractions]
+    losses = [results[fraction]['loss'] for fraction in fractions]
+
+    fig, ax1 = plt.subplots()
+
+    ax1.set_xlabel('Client Fraction')
+    ax1.set_ylabel('Accuracy', color='tab:blue')
+    ax1.bar(fractions, accuracies, color='tab:blue', alpha=0.6)
+    ax1.tick_params(axis='y', labelcolor='tab:blue')
+
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('Loss', color='tab:red')
+    ax2.plot(fractions, losses, color='tab:red', marker='o')
+    ax2.tick_params(axis='y', labelcolor='tab:red')
+
+    fig.tight_layout()
+    plt.savefig(plot_file)  # Save the plot to a file
+    plt.show()
+
 
 # Configuration
-parallel = 'parallel'
-model_path = './saved_models/global_model_round_125.h5'  # Adjust as needed
-test_data_path = './Assignment3-data/test_data.npy'    # Adjust as needed
-output_file = './evaluation_results_regular.txt'               # Output file path
+test_data_path = './Assignment3-data/test_data.npy'  # Adjust as needed
+output_file = './evaluation_results.txt'  # Output file path
 
-# Create an instance of the evaluator and run the evaluation
-evaluator = ModelEvaluator(model_path, test_data_path, output_file)
-evaluator.run_evaluation()
+# Client fractions and corresponding model paths
+client_fractions = [0.025, 0.05, 0.075, 0.1]
+model_paths = {fraction: f'./saved_models/fraction_{fraction}/global_model_round_125.h5' for fraction in client_fractions}
+
+# Clearing the output file
+open(output_file, 'w').close()
+
+# Evaluate each model
+evaluator = ModelEvaluator(test_data_path)
+evaluation_results = {}
+
+for fraction, model_path in model_paths.items():
+    test_loss, test_accuracy = evaluator.run_evaluation(model_path, output_file)
+    evaluation_results[fraction] = {'loss': test_loss, 'accuracy': test_accuracy}
+
+# Plotting and saving the results
+plot_file = './evaluation_plot.png'  # Path to save the plot
+plot_results(evaluation_results, plot_file)
--- 490_msia/03_hw/03_hw_q1_code.py
+++ 490_msia/03_hw/03_hw_q1_code.py
@@ -1,34 +1,37 @@
-# Use validation sets to evaluate on each client, then send back the train loss, acc, val loss, acc, and len of sample
-
 import numpy as np
 import random
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from collections import Counter
 import os
-from sklearn.utils.class_weight import compute_class_weight
 
 class FederatedLearningManager:
     def __init__(self, config):
         self.config = config
+        self.set_gpu(self.config['gpu_id'])
         self.train_data = np.load(config['train_data_path'], allow_pickle=True)
-        self.test_data = np.load(config['test_data_path'], allow_pickle=True)
-        self.X_test = np.array(self.test_data[0]['images'])
-        self.y_test = np.array(self.test_data[0]['labels'])
         self.client_train_val_data = self.prepare_client_data()
         self.global_model = self.create_model()
 
-    def create_model(self):
-        learning_rate = 0.001
+    def set_gpu(self, gpu_id):
+        gpus = tf.config.list_physical_devices('GPU')
+        if gpus:
+            try:
+                # Restrict TensorFlow to only use the specified GPU
+                tf.config.set_visible_devices(gpus[gpu_id], 'GPU')
+                tf.config.experimental.set_memory_growth(gpus[gpu_id], True)
+            except RuntimeError as e:
+                # Visible devices must be set before GPUs have been initialized
+                print(e)
 
+    def create_model(self):
         model = tf.keras.Sequential([
             tf.keras.layers.Flatten(input_shape=(28, 28)),
             tf.keras.layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
             tf.keras.layers.Dense(62, activation='softmax')  # Output layer for 62 classes
         ])
 
-        model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+        model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=self.config['learning_rate']),
                       loss='sparse_categorical_crossentropy',
                       metrics=['accuracy'])
         
@@ -64,7 +67,7 @@ class FederatedLearningManager:
         X_val = np.array(client_data['X_val'])
         y_val = np.array(client_data['y_val'])
 
-        history = model.fit(X_train, y_train, epochs=self.config['local_epochs'], batch_size=16, validation_data=(X_val, y_val), verbose=0)
+        history = model.fit(X_train, y_train, epochs=self.config['local_epochs'], batch_size=32, validation_data=(X_val, y_val), verbose=0)
 
         train_loss, train_accuracy = history.history['loss'][-1], history.history['accuracy'][-1]
         val_loss, val_accuracy = model.evaluate(X_val, y_val, verbose=0)
@@ -99,43 +102,45 @@ class FederatedLearningManager:
         
         return average_weights
 
-    def save_model(self, round):
-        model_path = os.path.join(self.config['model_save_dir'], f'global_model_round_{round+1}.h5')
+    def save_model(self, iteration):
+        model_path = os.path.join(self.config['model_save_dir'], f'global_model_round_{iteration+1}.h5')
         self.global_model.save(model_path)
         print(f'Model saved at {model_path}')
 
-    def plot_progress(self, history, round):
+    def plot_progress(self, history, iteration, client_fraction):
         plt.figure(figsize=(12, 10))
+        plt.suptitle(f'Client Fraction: {client_fraction}', fontsize=14)
+
         plt.subplot(2, 2, 1)
-        plt.plot(range(1, round + 2), history['train_accuracy'], label='Train Accuracy')
+        plt.plot(range(1, iteration + 2), history['train_accuracy'], label='Train Accuracy')
         plt.title('Train Accuracy')
         plt.xlabel('Rounds')
         plt.ylabel('Accuracy')
         plt.legend()
 
         plt.subplot(2, 2, 2)
-        plt.plot(range(1, round + 2), history['train_loss'], label='Train Loss')
+        plt.plot(range(1, iteration + 2), history['train_loss'], label='Train Loss')
         plt.title('Train Loss')
         plt.xlabel('Rounds')
         plt.ylabel('Loss')
         plt.legend()
 
         plt.subplot(2, 2, 3)
-        plt.plot(range(1, round + 2), history['val_accuracy'], label='Validation Accuracy')
+        plt.plot(range(1, iteration + 2), history['val_accuracy'], label='Validation Accuracy')
         plt.title('Validation Accuracy')
         plt.xlabel('Rounds')
         plt.ylabel('Accuracy')
         plt.legend()
 
         plt.subplot(2, 2, 4)
-        plt.plot(range(1, round + 2), history['val_loss'], label='Validation Loss')
+        plt.plot(range(1, iteration + 2), history['val_loss'], label='Validation Loss')
         plt.title('Validation Loss')
         plt.xlabel('Rounds')
         plt.ylabel('Loss')
         plt.legend()
 
         plt.tight_layout()
-        plot_path = os.path.join(self.config['plot_save_dir'], f'training_progress_round_{round+1}.png')
+        plot_path = os.path.join(self.config['plot_save_dir'], f'training_progress_round_{iteration+1}.png')
         plt.savefig(plot_path)
         print(f'Progress plot saved at {plot_path}')
         plt.close()
@@ -143,11 +148,10 @@ class FederatedLearningManager:
     def federated_training(self):
         global_history = {
             'train_accuracy': [], 'train_loss': [],
-            'val_accuracy': [], 'val_loss': [],
-            'test_accuracy': [], 'test_loss': []  # Added test metrics
+            'val_accuracy': [], 'val_loss': []
         }
 
-        for round in range(self.config['communication_rounds']):
+        for iteration in range(self.config['communication_rounds']):
             selected_clients_indices = self.select_clients()
             weights_list = []
             sample_sizes = []
@@ -169,48 +173,70 @@ class FederatedLearningManager:
             # Update global model with the average weights after all clients have been processed
             updated_weights = self.average_weights(weights_list, sample_sizes)
             self.global_model.set_weights(updated_weights)
-            
-            # Evaluate on test data after updating the global model
-            test_loss, test_accuracy = self.global_model.evaluate(self.X_test, self.y_test, verbose=0)
-            global_history['test_accuracy'].append(test_accuracy)
-            global_history['test_loss'].append(test_loss)
 
             # Calculate weighted average of metrics for the round
             for metric in ['train_accuracy', 'train_loss', 'val_accuracy', 'val_loss']:
                 global_history[metric].append(round_metrics[metric] / round_metrics['total_samples'])
 
-            # Print round summary after test evaluation
-            print(f'Round {round+1}, Train Loss: {round(global_history["train_loss"][-1], 4)}, '
+            # Print round summary
+            print(f'Round {iteration+1}, Train Loss: {round(global_history["train_loss"][-1], 4)}, '
                 f'Train Accuracy: {round(global_history["train_accuracy"][-1], 4)}, '
                 f'Validation Loss: {round(global_history["val_loss"][-1], 4)}, '
-                f'Validation Accuracy: {round(global_history["val_accuracy"][-1], 4)}, '
-                f'Test Loss: {round(test_loss, 4)}, Test Accuracy: {round(test_accuracy, 4)}'
+                f'Validation Accuracy: {round(global_history["val_accuracy"][-1], 4)}'
                 )
             
             # Save the model and plot at specified intervals
-            if (round + 1) % self.config['save_interval'] == 0:
-                self.save_model(round)
-                self.plot_progress(global_history, round)
+            if (iteration + 1) % self.config['save_interval'] == 0:
+                self.save_model(iteration)
+                self.plot_progress(global_history, iteration, self.config['client_fraction'])
                 
         return self.global_model, global_history
 
-
-# Configuration settings
-config = {
+#  Configuration settings common to all runs
+base_config = {
     'train_data_path': "./Assignment3-data/train_data.npy",
-    'test_data_path': "./Assignment3-data/test_data.npy",
-    'communication_rounds': 500,
-    'client_fraction': 0.1,
+    'communication_rounds': 1000,
     'local_epochs': 20,
-    'save_interval': 5,  # Save model and plot every 5 rounds
+    'learning_rate': 0.001,  # Specify the learning rate
+    'save_interval': 50,  # Save model and plot every 5 rounds
     'model_save_dir': './saved_models',  # Directory to save models
-    'plot_save_dir': './training_plots'  # Directory to save plots
+    'plot_save_dir': './training_plots',  # Directory to save plots
+    'gpu_id': 1  # Specify which GPU to use
 }
 
-# Ensure directories exist
-os.makedirs(config['model_save_dir'], exist_ok=True)
-os.makedirs(config['plot_save_dir'], exist_ok=True)
+# Client fractions to try
+client_fractions = [0.025, 0.05, 0.075, 0.1]
 
-# Initialize and run federated learning
-fl_manager = FederatedLearningManager(config)
-global_model, training_history = fl_manager.federated_training()
+# Dictionary to store final results for each client fraction
+final_results = {}
+
+# Loop over each client fraction
+for fraction in client_fractions:
+    # Update client fraction in configuration
+    config = base_config.copy()
+    config['client_fraction'] = fraction
+
+    # Update directories to include client fraction
+    fraction_dir = f'fraction_{fraction}'
+    config['model_save_dir'] = os.path.join('./saved_models', fraction_dir)
+    config['plot_save_dir'] = os.path.join('./training_plots', fraction_dir)
+
+    # Ensure directories exist
+    os.makedirs(config['model_save_dir'], exist_ok=True)
+    os.makedirs(config['plot_save_dir'], exist_ok=True)
+
+    # Initialize and run federated learning
+    fl_manager = FederatedLearningManager(config)
+    global_model, training_history = fl_manager.federated_training()
+
+    # Store the final results
+    final_results[fraction] = {
+        'model': global_model,
+        'history': training_history
+    }
+
+# Output final results
+for fraction, results in final_results.items():
+    print(f"Results for client fraction {fraction}:")
+    print(f"Training History: {results['history']}")
+    
